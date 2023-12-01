6577b283af4cadcbfc1c4c67df4033266c448675
Dylan Claywell <djclaywell@gmail.com>
2023-12-01 12:21:45 -0600

    Added confirmation for resetting the project

--- src/components/ConfirmationModal.vue
+++ src/components/ConfirmationModal.vue
@@ -16,7 +16,26 @@ const props = withDefaults(defineProps<Props>(), {
 
 <template>
   <div>
-    <div class="fixed top-0 left-0 w-screen h-screen blur"></div>
-    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">Hello world</div>
+    <div class="fixed top-0 left-0 w-screen h-screen z-50 bg-gray-800 opacity-50"></div>
+    <div
+      class="p-4 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 bg-white rounded-md shadow-md space-y-4 max-w-md"
+    >
+      <h1 class="text-xl font-bold">{{ title }}</h1>
+      <p>{{ message }}</p>
+      <div class="flex justify-end space-x-2">
+        <button
+          class="p-2 hover:bg-gray-200 rounded-md uppercase tracking-wide font-bold"
+          @click="onCancel"
+        >
+          {{ cancelText }}
+        </button>
+        <button
+          class="p-2 bg-blue-600 hover:bg-blue-700 text-white rounded-md tracking-wide uppercase font-bold"
+          @click="onConfirm"
+        >
+          {{ confirmText }}
+        </button>
+      </div>
+    </div>
   </div>
 </template>
--- src/views/ToolbarView.vue
+++ src/views/ToolbarView.vue
@@ -1,12 +1,40 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
 import ToolBar from '../components/ToolBar/ToolBar.vue'
 import ToolSection from '../components/ToolBar/ToolSection.vue'
 import Tool from '../components/ToolBar/ToolButton.vue'
+import ConfirmationModal from '../components/ConfirmationModal.vue'
 import { useEditorStore } from '@/stores/editor'
+
+const editorStore = useEditorStore()
+
+const confirmationModalIsOpen = ref(false)
+
+function openConfirmationModal() {
+  confirmationModalIsOpen.value = true
+}
+
+function closeConfirmationModal() {
+  confirmationModalIsOpen.value = false
+}
+
+function onConfirm() {
+  editorStore.reset()
+  closeConfirmationModal()
+}
 </script>
 
 <template>
   <div class="w-full border-b border-b-gray-200">
+    <ConfirmationModal
+      v-if="confirmationModalIsOpen"
+      title="Create new project"
+      message="Are you sure you want to create a new project? Make sure to export this project so you don't lose it."
+      @confirm="onConfirm"
+      @cancel="closeConfirmationModal"
+      confirm-text="Create"
+    />
     <ToolBar>
       <ToolSection>
         <Tool
@@ -14,7 +42,7 @@ import { useEditorStore } from '@/stores/editor'
           icon="folder-plus"
           :is-disabled="false"
           :is-selected="false"
-          :on-click="() => useEditorStore().reset()"
+          :on-click="openConfirmationModal"
         />
         <Tool name="Save Project" icon="save" :is-disabled="false" :is-selected="false" />
       </ToolSection>
