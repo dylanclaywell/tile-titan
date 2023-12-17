89093d9d8896a6b9dc45c23c1e008443737bf439
Dylan Claywell <djclaywell@gmail.com>
2023-12-17 16:19:16 -0600

    Started adding the way to import a project

--- src/views/ToolbarView.vue
+++ src/views/ToolbarView.vue
@@ -6,6 +6,8 @@ import ToolSection from '../components/ToolBar/ToolSection.vue'
 import Tool from '../components/ToolBar/ToolButton.vue'
 import ConfirmationModal from '../components/ConfirmationModal.vue'
 import { useEditorStore } from '@/stores/editor'
+import { importProject } from '@/lib/importProject'
+import FileUploader from '@/components/FileUploader.vue'
 
 const editorStore = useEditorStore()
 
@@ -22,6 +24,35 @@ function closeConfirmationModal() {
 function onConfirm() {
   editorStore.reset()
   closeConfirmationModal()
+}
+
+async function readZipFile(file: File) {
+  return new Promise<string | ArrayBuffer | null | undefined>((resolve) => {
+    const reader = new FileReader()
+    reader.onload = (event) => {
+      resolve(event.target?.result)
+    }
+    reader.readAsBinaryString(file)
+  })
+}
+
+async function onUpload(event: Event) {
+  if (!(event.target instanceof HTMLInputElement)) return
+
+  const file = event.target.files?.[0]
+
+  if (!file) return
+
+  const blob = await readZipFile(file)
+
+  if (!blob) return
+
+  const { files, tilesets } = await importProject(blob)
+
+  editorStore.setFiles(files)
+  editorStore.setTilesets(tilesets)
+
+  event.target.value = ''
 }
 </script>
 
@@ -47,7 +78,13 @@ function onConfirm() {
         <Tool name="Save Project" icon="save" :is-disabled="false" :is-selected="false" />
       </ToolSection>
       <ToolSection>
-        <Tool name="Import Project" icon="upload" :is-disabled="false" :is-selected="false" />
+        <FileUploader name="Import Project" @change="onUpload">
+          <div
+            className="w-10 h-10 cursor-default hover:bg-gray-200 hover:border hover:border-gray-300 rounded-md flex justify-center items-center"
+          >
+            <i className="fa-solid fa-upload"></i>
+          </div>
+        </FileUploader>
         <Tool name="Export Project" icon="download" :is-disabled="false" :is-selected="false" />
       </ToolSection>
     </ToolBar>
